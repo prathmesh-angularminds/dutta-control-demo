4a89f722e4632ffe33edb2a6c80ad92df8c4219e
Prathamesh vibhute <prathmesh.vibhute@angularminds.in>
2023-01-30 11:58:37 +0530

    file Structure

---
+++ src/app/layouts/layouts.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppComponent } from './app/app.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LayoutsModule { }
---
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EditCustomerComponent } from './edit-customer/edit-customer.component';
+
+
+
+@NgModule({
+  declarations: [
+    EditCustomerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DashboardModule { }
