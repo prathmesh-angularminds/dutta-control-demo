e075aa3260eea40e839a9cc7318826b34283efc4
minds-rohitgkwd <rohit@angularminds.in>
2023-01-30 17:28:28 +0530

    updated customer edit form

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -1,28 +1,34 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from 'src/app/layouts/app/app.component';
-import { EditCustomerComponent } from './edit-customer/edit-customer.component';
+import { Component, NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from "src/app/layouts/app/app.component";
+import { AddressComponent } from "./edit-customer/address/address.component";
+import { ContactPersonsComponent } from "./edit-customer/contact-persons/contact-persons.component";
+import { EditCustomerComponent } from "./edit-customer/edit-customer.component";
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: '/dashboard',
-    pathMatch: 'full'
+    path: "",
+    redirectTo: "dashboard",
+    pathMatch: "full",
   },
   {
-    path: 'dashboard',
+    path: "dashboard",
     component: AppComponent,
     children: [
       {
-        path: '',
-        component: EditCustomerComponent
-      }
-    ]
-  }
+        path: "",
+        component: EditCustomerComponent,
+        children: [
+          { path: "address", component: AddressComponent },
+          { path: "contact-persons", component: ContactPersonsComponent },
+        ],
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class DashboardRoutingModule { }
+export class DashboardRoutingModule {}
--- src/app/modules/dashboard/edit-customer/edit-customer.component.ts
+++ src/app/modules/dashboard/edit-customer/edit-customer.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-edit-customer',
-  templateUrl: './edit-customer.component.html',
-  styleUrls: ['./edit-customer.component.css']
+  selector: "app-edit-customer",
+  templateUrl: "./edit-customer.component.html",
+  styleUrls: ["./edit-customer.component.css"],
 })
 export class EditCustomerComponent implements OnInit {
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
+  customerForm!: FormGroup;
 
   ngOnInit(): void {
+    this.customerForm = this.fb.group({
+      customerType: new FormControl(),
+      primaryContact: new FormGroup({
+        title: new FormControl(),
+        firstName: new FormControl(),
+        lastName: new FormControl(),
+      }),
+      companyName: new FormControl(),
+      customerDisplayName: new FormControl(),
+      email: new FormControl(),
+      phone: new FormGroup({
+        work: new FormControl(),
+        mobile: new FormControl(),
+      }),
+      website: new FormControl(),
+    });
+  }
+
+  submitForm(){
+    console.log(this.customerForm.value);
+    
   }
 
 }
