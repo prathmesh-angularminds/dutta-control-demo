37d6bbaa18ef719250490940eca665e214214631
Prathamesh vibhute <prathmesh.vibhute@angularminds.in>
2023-01-30 17:42:16 +0530

    Combine Merge

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,21 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { EditCustomerComponent } from './edit-customer/edit-customer.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { RouterModule } from '@angular/router';
 import { LayoutsModule } from 'src/app/layouts/layouts.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { EditCustomerModule } from './edit-customer/edit-customer.module';
 
 
 
 @NgModule({
-  declarations: [
-    EditCustomerComponent
-  ],
+  declarations: [],
   imports: [
     CommonModule,
     RouterModule,
+    EditCustomerModule,
     LayoutsModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class DashboardModule { }
