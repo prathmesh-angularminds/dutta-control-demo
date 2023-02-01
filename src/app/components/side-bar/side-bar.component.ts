81e6d8a04bd016568cbd171f1b91c2b515269651
Prathamesh vibhute <prathmesh.vibhute@angularminds.in>
2023-02-01 10:41:12 +0530

    Side bar changes

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
 
+  rotateBtn: any = {
+    activeBtn: ''
+  }
   constructor() { }
 
   ngOnInit(): void {
